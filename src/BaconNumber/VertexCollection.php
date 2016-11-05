59a7d04e3135b9e87b805d1d0d88b93cf9080f1d
Lukas Hajdu <hello@lukashajdu.com>
2016-11-05 13:54:41 +0000

    Make vertex collection iterable

--- src/BaconNumber/VertexCollection.php
+++ src/BaconNumber/VertexCollection.php
@@ -2,6 +2,9 @@
 
 namespace BaconNumber;
 
+use Countable;
+use Iterator;
+
 /**
  * Vertex Collection
  *
@@ -9,7 +12,7 @@ namespace BaconNumber;
  * @copyright Lukas Hajdu <http://lukashajdu.com>, 2016
  * @package BaconNumber
  */
-class VertexCollection
+class VertexCollection implements Countable, Iterator
 {
     /**
      * An array containing all vertex edges
@@ -54,5 +57,53 @@ class VertexCollection
     public function itemExists(VertexInterface $item): bool
     {
         return $this->keyExists($item->getHash());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function count()
+    {
+        return count($this->edges);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function key()
+    {
+        return key($this->edges);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function next()
+    {
+        return next($this->edges);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function current()
+    {
+        return current($this->edges);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function valid()
+    {
+        return key($this->edges) !== null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rewind()
+    {
+        reset($this->edges);
     }
 }
--- tests/BaconNumber/VertexCollectionTest.php
+++ tests/BaconNumber/VertexCollectionTest.php
@@ -39,4 +39,84 @@ class VertexCollectionTest extends TestCase
         $this->assertTrue($collection->add($vertex));
         $this->assertTrue($collection->itemExists($vertex));
     }
+
+    /**
+     * @covers \BaconNumber\VertexCollection::count
+     */
+    public function testCount()
+    {
+        $collection = new VertexCollection();
+
+        $collection->add(new Vertex('A'));
+        $collection->add(new Vertex('A'));
+        $this->assertEquals(1, $collection->count());
+
+        $collection->add(new Vertex('B'));
+        $this->assertEquals(2, $collection->count());
+    }
+
+    /**
+     * @covers \BaconNumber\VertexCollection::rewind
+     * @covers \BaconNumber\VertexCollection::key
+     */
+    public function testRewind()
+    {
+        $vertex1 = new Vertex('A');
+        $vertex2 = new Vertex('B');
+        $collection = new VertexCollection();
+        $collection->add($vertex1);
+        $collection->add($vertex2);
+
+        $collection->rewind();
+        $this->assertEquals($vertex1->getHash(), $collection->key());
+        $collection->next();
+        $collection->rewind();
+        $this->assertEquals($vertex1->getHash(), $collection->key());
+    }
+
+    /**
+     * @covers \BaconNumber\VertexCollection::next
+     */
+    public function testNext()
+    {
+        $vertex1 = new Vertex('A');
+        $vertex2 = new Vertex('B');
+        $collection = new VertexCollection();
+        $collection->add($vertex1);
+        $collection->add($vertex2);
+
+        $collection->rewind();
+        $this->assertEquals($vertex2, $collection->next());
+    }
+
+    /**
+     * @covers \BaconNumber\VertexCollection::current
+     */
+    public function testCurrent()
+    {
+        $vertex1 = new Vertex('A');
+        $vertex2 = new Vertex('B');
+        $collection = new VertexCollection();
+        $collection->add($vertex1);
+        $collection->add($vertex2);
+
+        $collection->rewind();
+        $this->assertEquals($vertex1, $collection->current());
+        $collection->next();
+        $this->assertEquals($vertex2, $collection->current());
+    }
+
+    /**
+     * @covers \BaconNumber\VertexCollection::valid
+     */
+    public function testValid()
+    {
+        $vertex = new Vertex('A');
+        $collection = new VertexCollection();
+        $collection->add($vertex);
+
+        $this->assertTrue($collection->valid());
+        $collection->next();
+        $this->assertFalse($collection->valid());
+    }
 }
